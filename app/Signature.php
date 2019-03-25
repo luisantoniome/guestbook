a2a460156fe7c45ab7190c1a2497b3055c52b207
Luis Antonio Mendoza <luismendoza.dev@gmail.com>
2019-03-25 15:44:13 -0500

    Signature Controller and transformer

--- app/Signature.php
+++ app/Signature.php
@@ -7,4 +7,25 @@ use Illuminate\Database\Eloquent\Model;
 class Signature extends Model
 {
     protected $fillable = ['name', 'email', 'body', 'flagged_at'];
+
+    /**
+     * Ignore flagged signatures.
+     * 
+     * @param $query
+     * @return mixed
+     */
+    public function scopeIgnoreFlagged($query)
+    {
+        return $query->where('flagged_at', null);
+    }
+
+    /**
+     * Flag the given signature.
+     * 
+     * @return bool
+     */
+    public function flag()
+    {
+        return $this->update(['flagged_at' => \Carbon\Carbon::now()]);
+    }
 }
